d9b6fb454e5f14f88cc38cc24e451415a763360c
wow dancers! <emailspammerv1@gmail.com>
2024-02-15 07:22:03 +0000

    boom

---
+++ main.py
@@ -0,0 +1,40 @@
+import json
+from typing import Dict, List, Union
+from colorama import Fore, Style
+
+from modules.process import process_usernames
+from modules.request import _Session
+
+
+class UsernameChecker:
+    def __init__(self, _config: str = "./data/config.json"):
+        self.config: Dict[str, Union[bool, Dict[str, str]]
+                          ] = json.load(open(_config))
+        self.use_proxies: bool = self.config.get("use_proxies", True)
+
+        self.session: _Session = _Session(use_proxies=self.use_proxies)
+        self.proxy: Dict[str, str] = self.config.get(
+            "proxy", {}) if self.use_proxies else {}
+
+        self.usernames: List[str] = process_usernames(self.config["usernames"])
+        self.usernames_to_check: List[str] = open(
+            self.config["usernames"]).read().splitlines()
+
+    def check_usernames(self) -> List[str]:
+        return [self.format_output(username, self.check(username)) for username in self.usernames_to_check]
+
+    def check(self, username: str) -> bool:
+        return self.session.proxy_check(username, self.proxy) if self.use_proxies else self.session.check_user(username)
+
+    def format_output(self, username: str, result: bool) -> str:
+        status: str = "available" if result else "taken"
+        color: str = Fore.GREEN if result else Fore.RED
+        return f"[{color}{username}{Style.RESET_ALL}] - {status}"
+
+
+if __name__ == "__main__":
+    checker: UsernameChecker = UsernameChecker()
+    results: List[str] = checker.check_usernames()
+
+    for result in results:
+        print(result)
---
+++ modules/request.py
@@ -0,0 +1,35 @@
+from typing import Dict
+
+import requests
+
+
+class _Session(requests.Session):
+    def __init__(self, use_proxies: bool = False):
+        super().__init__()
+        self.configure_proxies(use_proxies)
+
+    def configure_proxies(self, use_proxies: bool):
+        if use_proxies:
+            self.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+            self.mount('https://', requests.adapters.HTTPAdapter(max_retries=3))
+
+    def check_username(self, username: str) -> bool:
+        return self.get(f'https://api.github.com/users/{username}').status_code == 404
+
+    def proxy_check(self, username: str, proxy: Dict[str, str]) -> bool:
+        if not proxy:
+            raise ValueError(
+                "Proxy configuration is required for proxy support.")
+
+        adapter = self.get_adapter("http://")
+        if not isinstance(adapter, requests.adapters.HTTPAdapter):
+            raise ValueError("Proxy support requires the HTTPAdapter.")
+
+        setattr(adapter, 'proxy_manager',
+                self.adapters['http://'].proxy_manager)
+
+        return self.get(f'https://api.github.com/users/{username}', proxies=proxy).status_code == 404
+
+    def set_request_headers(self, headers: Dict[str, str]):
+        for key, value in headers.items():
+            setattr(self.headers, key, value)
