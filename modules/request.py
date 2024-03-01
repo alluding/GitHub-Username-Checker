7547f96f3cf9ac9bc129d7fdf994de3c439278d8
paranoid <149972831+alluding@users.noreply.github.com>
2024-03-01 15:39:42 -0500

    Use base URL, and API (lower rate limits).

--- modules/request.py
+++ modules/request.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Union
 
 import requests
 
@@ -12,13 +12,14 @@ class _Session(requests.Session):
             self.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
             self.mount('https://', requests.adapters.HTTPAdapter(max_retries=3))
 
-    def check_username(self, username: str) -> bool:
-        return self.get(f'https://api.github.com/users/{username}').status_code == 404
+    def check_username(self, username: str) -> Union[bool, str]:
+        html, api = (self.get(url.format(username)).status_code for url in ['https://github.com/{}', 'https://api.github.com/users/{}'])
 
-    def proxy_check(self, username: str, proxy: Dict[str, str]) -> bool:
+        return True if html == 404 and (api := self.get(f'https://api.github.com/users/{username}').status_code) == 404 else False if html == 200 else "Rate limited!" if html == 403 else None
+
+    def proxy_check(self, username: str, proxy: Dict[str, str]) -> Union[bool, str]:
         if not proxy:
-            raise ValueError(
-                "Proxy configuration is required for proxy support.")
+            raise ValueError("Proxy configuration is required for proxy support.")
 
         adapter = self.get_adapter("http://")
         if not isinstance(adapter, requests.adapters.HTTPAdapter):
@@ -27,7 +28,9 @@ class _Session(requests.Session):
         setattr(adapter, 'proxy_manager',
                 self.adapters['http://'].proxy_manager)
 
-        return self.get(f'https://api.github.com/users/{username}', proxies=proxy).status_code == 404
+        html, api = (self.get(url.format(username), proxies=proxy).status_code for url in ['https://github.com/{}', 'https://api.github.com/users/{}'])
+
+        return True if html == 404 and (api := self.get(f'https://api.github.com/users/{username}', proxies=proxy).status_code) == 404 else False if html == 200 else "Rate limited!" if html == 403 else None
 
     def set_request_headers(self, headers: Dict[str, str]):
         for key, value in headers.items():
