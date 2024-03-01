74f6fd965c0c620c20c5b80dacf7621c6a4574fa
paranoid <149972831+alluding@users.noreply.github.com>
2024-03-01 15:39:12 -0500

    Handle rate limits.

--- main.py
+++ main.py
@@ -15,20 +15,20 @@ class UsernameChecker:
         self.proxy: Dict[str, str] = self.config.get("proxy", {}) if self.use_proxies else {}
 
         self.usernames: List[str] = process_usernames(self.config["usernames"])
-        self.usernames_to_check: List[str] = open(self.config["usernames"]).read().splitlines()
+        self._usernames: List[str] = open(self.config["usernames"]).read().splitlines()
 
     def check_usernames(self) -> List[str]:
-        return [self.format_output(username, self.check(username)) for username in self.usernames_to_check]
+        return [self._format(username, self._check(username)) for username in self._usernames]
 
-    def check(self, username: str) -> bool:
-        result = self.session.proxy_check(
-            username, self.proxy) if self.use_proxies else self.session.check_user(username)
+    def _check(self, username: str) -> bool:
+        result = self.session.proxy_check(username, self.proxy) if self.use_proxies else self.session.check_username(username)
         print(f"{self._format(username, result)}\n", end='', flush=True)
+        return result
 
-    def _format(self, username: str, result: bool) -> str:
-        status: str = "available" if result else "taken"
-        color: str = Fore.GREEN if result else Fore.RED
-        return f"[{color}{username}{Style.RESET_ALL}] - {status}"
+    def _format(self, username: str, result: Union[str, bool]) -> str:
+        status = "available" if isinstance(result, bool) and result else "taken"
+        color = Fore.GREEN if result else Fore.RED
+        return f"[{color}{username}{Style.RESET_ALL}] - {status}" if isinstance(result, bool) else f"[{Fore.YELLOW}{username}{Style.RESET_ALL}] - {result}" if "Rate" in result else f"[{Fore.YELLOW}{username}{Style.RESET_ALL}] - Unexpected result: {result}"
 
 if __name__ == "__main__":
     checker: UsernameChecker = UsernameChecker()
